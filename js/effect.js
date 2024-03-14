b98442d72724f4a24ae01c53c55fa28f0470acf9
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-03-14 23:25:04 +0100

    Add files via upload

--- js/effect.js
+++ js/effect.js
@@ -1,1 +1,26 @@
-// nice paralax effect for the site 
+
+var picture = document.getElementById('paralax-picture');
+
+
+;(function(){
+
+  var throttle = function(type, name, obj){
+    var obj = obj || window;
+    var running = false;
+    var func = function(){
+      if (running){ return; }
+      running = true;
+      requestAnimationFrame(function(){
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+    obj.addEventListener(type, func);
+  };
+
+  throttle("scroll", "optimizedScroll");
+})();
+
+window.addEventListener("optimizedScroll", function(){
+    picture.style.backgroundPositionY = (window.pageYOffset * 0.05) + "%";
+})
